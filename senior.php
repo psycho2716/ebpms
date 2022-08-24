2b3df3fd098765c46ec2b342559a275eb26749b7
psycho27 <manzoej@gmail.com>
2022-08-25 06:49:54 +0800

    Update senior.php

--- senior.php
+++ senior.php
@@ -149,7 +149,7 @@ if (isset($_POST['edit_resident'])) {
                 </thead>
                 <tbody>
                     <?php
-                    $sql = "SELECT * FROM residents";
+                    $sql = "SELECT * FROM residents WHERE barangay_id = '$barangay_id'";
                     $run_query = mysqli_query($conn, $sql);
                     $i = 0;
 
